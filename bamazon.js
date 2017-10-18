74bc09f7ce73d920ea3de8b07f3688755d34e7f1
Jim Reinknecht <captainjimscharters@gmail.com>
2017-10-18 13:25:07 -0400

    pop

---
+++ bamazon.js
@@ -0,0 +1,28 @@
+var fs=require('fs');
+var mysql=require('mysql');
+var inquirer=require('inquirer');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  database : 'bamazon_db'
+});
+
+var prompt = {
+one: {
+
+},
+
+two: {
+	
+
+}.
+three: {
+
+
+},
+four: {
+
+
+}
+}
+
