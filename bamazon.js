973766bbd21f0481b5a613da572a7d6995951d88
Jim Reinknecht <captainjimscharters@gmail.com>
2017-10-21 06:41:08 -0400

    jjjjj

--- bamazon.js
+++ bamazon.js
@@ -7,44 +7,51 @@ var connection = mysql.createConnection({
     database: 'bamazon_db'
 });
 var inq=inquirer;
+var Table = require ('easy-table');
 
-var questions = {
+// prompt questions
+
+var questions = {name:{
 	type: "input",
 	message: "Howdy pard'ner. What be yer name ya go by?",
 	name: "name"
 	},
-	{
+	menu:{
     type: "list",
     name: "menuChoice",
-    message: "Welcome to Bamazon.  Your Last Stop For Provisions on The BORE-Egon Trail. What will you do?",
+    message: "Welcome to Bamazon. Your Last Stop For Provisions on The BORE-Egon Trail. What will you do?",
     choices: ["Buy a Product","Search for a Product","Depart On The Trail"]
     },
-	{
+	buy:{
     type: "input",
     name: "buy", 	
-    message: "Which product would yeh like to buy?"
+    message: "Which product would yeh like to buy?\n"
     },
-   {
+   quant:{
     type: "input",
     name: "buyQuantity",
-    message: "How Many?"
-    };
+    message: "How Many?\n"
+    }
+};
 
 var cash,
-	name;
+	name,
+	 allProducts;
 
 //new game
+
 function newGame(){
  cash=800;
- inq.prompt(questions.name).then(answer => {
-  answer.name=name;
-  
+ //enter name
+ inq.prompt(questions.name).then((answer) => {
+ name=answer.name;
+ console.log(name);
+ mainMenu();
 
  })
 
 }
-//enter name
-//enter store
+
 
   //purchase a product
 	//list the products
@@ -65,27 +72,112 @@ function newGame(){
 //list all the profucts
 
 function listAll() {
- connection.query('SELECT id, name, description, price, quantity_on_hand FROM products', function (error, results, fields) {
+	var t=new Table
+ connection.query('SELECT * FROM products', function (error, results, fields) {
   if (error) throw error;
-console.log(results)
+//console.log(results)
+results.forEach ((RowDataPacket)=>{
+t.cell('ID',RowDataPacket.id);
+t.cell('Product',RowDataPacket.name);
+t.cell('Description',RowDataPacket.description);
+t.cell('Price',RowDataPacket.price);
+t.cell('Category', RowDataPacket.category);
+t.cell('Quantity On Hand',RowDataPacket.quantity_on_hand);
+t.newRow();
+})
+console.log(t.toString());
 });
  connection.end();
+ //mainMenu();
 }
-//listAll();
+
+function mainMenu(){
+inq.prompt(questions.menu).then((answer)=> {
+
+switch(answer.menuChoice){
+
+	case "Buy a Product":
+		console.log("BUY");
+		console.log("answer.menuChoice");
+		buyStuff();
+		//mainMenu();
+		break;
+
+	case "Search for a Product":
+				console.log("SEARCH");
+		console.log("answer.menuChoice");
+		//searchStuff();
+		mainMenu();
+		break;
+	case "Depart On The Trail":
+			console.log("DEPART");
+		console.log("answer.menuChoice");
+		//trailStuff()
+		break;
+}
+
+})
+
+
+}
+
+function buyStuff(){
+
+//enter store
+	//find out which item they want
+listAll();
+
+inq.prompt(questions.buy).then((answer)=>{
+
+	var buyItemID=[answer.buy];
+	//buyItemID.push()
+	//;
+	console.log(buyItemID);
+	// get quantity
+
+		inq.prompt(questions.quant).then((ans)=>{
+			var buyItemQuant=ans.buyQuantity;
+				connection.query('SELECT * FROM products WHERE id=?',[buyItemID], (error, results) => {
+  					if (error) throw error;
+
+  					console.log(results);
+  				})
+  				connection.end();
+				//if (!checkQuantity())
+			 //check to see if that amount exists in db
+			 	// if it doesnt, 
+			 		//alert the user that only X items exist, rerun buyStuff();
+			 	//if it does
+			 		// calc price, confirm with user. 
+			 			//make sure user has enough money
+			 				//if yes 
+			 					//deduct money total from cash
+			 					//deduct item from database quantity_on_hand
+			 					//add order to orders
 
 
 
+		})
+	})
+
+}
+
+function checkQuantity(){
+
+}
+
+function updateQuantity(id,quant){
+	//connection.query('SELECT quantity_on_hand FROM products WHERE id='+buyItemID,  (error, results, fields) => {
+  		connection.query('SELECT quantity_on_hand FROM products WHERE id=3',  (error, results, fields) => {
+
+  	if (error) throw error;
+  		console.log(results)
 
 
-// connection.connect(function(err) {
-//   if (err) throw err;
-//   connection.query("SELECT id, product_name, price FROM products", function (err, result, fields) {
-//     if (err) throw err;
-//     console.log("WEELCOME, GOT SOME THINGS THINGS THAT MIGHT INTEREST YOU!");
-//     console.log("\n //==================================================================\\ \n");
-//     console.log(result);
-//     console.log("\n \\=================================================================//");
-//     questions();
-//   });
-// });
+  	})
+  		connection.end();
+}
 
+
+
+buyStuff()
